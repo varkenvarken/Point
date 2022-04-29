a84cb04ad1c96a30e53c76b6290da8b520c87283
varkenvarken <michel@michelanders.nl>
2022-04-29 14:08:52 +0200

    tweak

--- src/point/server.py
+++ src/point/server.py
@@ -24,7 +24,8 @@ class Server(HTTPServer):
                     self.pc = PointCollection.loads(config, pwm=pwm)
                 except JSONDecodeError as e:
                     raise ValueError(f"could not correctly read config file {e}")
-        if len(self.pc) == 0:  # we started with an empty database
+        else:  # we started with an empty database
+            self.pc = PointCollection(pwm=pwm)
             point0 = Point(0, "Point at port 0", pwm=pwm)
             self.pc[point0.index] = point0
             self.writeDBfile()
